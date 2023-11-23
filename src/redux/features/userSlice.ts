f1953418ae77d6e2c781c765250e6983940718df
yutaokkots <yuta.okkotsu@gmail.com>
2023-11-22 19:23:07 -0700

    Updated notes on slice in redux_notes.txt; included code for an example slice, userSlice.

--- src/redux/features/userSlice.ts
+++ src/redux/features/userSlice.ts
@@ -1,15 +1,26 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+export const fetchUsers = createAsyncThunk('users/getAllUsers', async(thunkAPI) => {
+    const response = await fetch('url')
+    const data = await response.json();
+    return data;
+})
+
 type UserState = {
-    value: string;
+    entities: string[];
   };
 
 const initialState: UserState = {
-    value: '',
+    entities: [],
 }
 
 const userSlice = createSlice({
     name: 'users',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.entities.push(...action.payload)
+        })
+    }
 })
