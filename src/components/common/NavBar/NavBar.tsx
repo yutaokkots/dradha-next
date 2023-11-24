ce57e1b2fbb741eb31b70b6bf381943aaad61b51
yutaokkots <yuta.okkotsu@gmail.com>
2023-11-24 11:02:50 -0700

    Styling the navbar to include hamburger and left-fixed bar.

--- src/components/common/NavBar/NavBar.tsx
+++ src/components/common/NavBar/NavBar.tsx
@@ -1,16 +1,35 @@
-import React from 'react'
-import ThemeToggler from '@/components/primitives/ThemeToggler'
+import React from 'react';
+import ThemeToggler from '@/components/primitives/ThemeToggler';
+import Hamburger from './Hamburger';
 
-const NavBar:React.FC = () => {
+interface NavBarProps {
+    // Space for additional props
+}
+
+const NavBar:React.FC<NavBarProps> = () => {
     return(
-        <nav className="h-screen md:flex md:w-[15vw] md:fixed flex-col md:items-start z-10 border-2 border-red-200">
-            <ThemeToggler />
-            <div>Name</div>
-            <div>Home</div>
-            <div>Self</div>
-            <div>Project</div>
-            <div>Sign in</div>
-        </nav>
+        <div className="h-[75px] w-screen fixed top-0 z-10 border-2 border-red-200 
+                smcustom:h-screen smcustom:w-[150px] smcustom:left-0 smcustom:border-2 smcustom:border-green-500">
+            
+            <div className="">
+                <div className="smcustom:hidden h-full flex flex-1 justify-end border-2 border-yellow-200 ">
+                    <div className="h-full  flex center border-2 border-blue-200 ">
+                        <Hamburger/>
+                    </div>
+                </div>
+                
+            </div>
+            <nav>
+                <div className="hidden smcustom:visible smcustom:fixed smcustom:flex smcustom:flex-col smcustom:items-start smcustom:w-[15vw]">
+                    <ThemeToggler />
+                    <div>Name</div>
+                    <div>Home</div>
+                    <div>Self</div>
+                    <div>Project</div>
+                    <div>Sign in</div>
+                </div>
+            </nav>
+        </div>
     )
 }
 
